0b37e90bdaf0c36ac07a700c8c3317d1a588a0f0
anggisaputraardi <anggiardi99@gmail.com>
2020-06-01 11:53:24 +0700

    Syafra Photography

---
+++ js/script.js
@@ -0,0 +1,23 @@
+ $(".button-collapse").sideNav();
+
+ $(document).ready(function(){
+      $('.slider').slider({
+      	indicators:false,
+      	height:600,
+      	transition:600,
+      	interval:3000
+      	});
+    });
+
+
+//materialbox
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+
+//scrollspy
+ $(document).ready(function(){
+    $('.scrollspy').scrollSpy({
+    	scrollOffset:50
+    });
+  });
